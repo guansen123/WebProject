a3c5e51bf986aaed60fba38a15dd34e119b18022
guansen123 <kacang0324@gmail.com>
2021-03-17 12:55:06 +0800

    123

---
+++ phphw1.php
@@ -0,0 +1,30 @@
+
+/*<?php
+ $x=1;
+ function hello($x){
+      $sum=$x * 2;
+      echo "sum=".$sum. "函數執行結束<br/>";
+      return $sum;
+    }
+?>
+<p>----------我是分割線------ <p>
+<?php
+     $sum=hello($x);
+     echo"x=$x<br/>";
+     echo"sum=$sum<br/>";*/
+
+    //Circule rate
+    define('PI',3.1415926);
+
+    //Circule radius
+    $radius=10;
+    //Caculate Circule Area
+    $area =$radius * $radius*PI;
+    echo"Circule area is".$area."\n";
+
+   //cacule circule length
+   $length =$radius * 2 *PI;
+   echo"Circule length is".$length."\n";
+
+   
+
---
+++ phphw2.php
@@ -0,0 +1,6 @@
+?php
+   $pi = 3.141592;
+   echo "\$pi 轉成整數 : ".(int)$pi;
+   $input = "3.141592pi";
+   echo "pi 轉成浮點數 : ".(float)$input;
+?>
